357305c5a07ac1f2508a57b256157075e5b051db
Igor Martins <73676465+igormpmartins@users.noreply.github.com>
2021-09-11 11:05:44 -0300

    Delete dist directory

--- dist/database/mysql.provider.d.ts
+++
@@ -1,7 +0,0 @@
-import { Connection } from 'mysql2/promise';
-export declare class MySQLProvider {
-    private readonly logger;
-    private readonly pool;
-    constructor();
-    getConnection(): Promise<Connection>;
-}
--- dist/modules/product.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { ProductService } from './product/product.service';
-export declare class ProductController {
-    private readonly productService;
-    constructor(productService: ProductService);
-    findAll(): Promise<any>;
-}
